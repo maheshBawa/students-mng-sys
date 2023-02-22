7d5e9be4fc8d1d517b5ddd885e025930094d5151
maheshBawa <mahesh.bawantha@revocaresolutions.com>
2023-02-22 20:24:25 +0530

    formated

--- src/main/java/com/student/student/StudentConfig.java
+++ src/main/java/com/student/student/StudentConfig.java
@@ -11,27 +11,24 @@ import org.springframework.context.annotation.Configuration;
 public class StudentConfig {
 
     @Bean
-    CommandLineRunner commandLineRunner(StudentRepository repository){
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
             Student mahesh = new Student(
 
-				1L, 
-                "Lahiru",
-				LocalDate.of(1998, Month.MAY, 31),
-				"maheshbawantha1998@gmail.com"
-			);
+                    1L,
+                    "Lahiru",
+                    LocalDate.of(1998, Month.MAY, 31),
+                    "maheshbawantha1998@gmail.com");
 
             Student bawantha = new Student(
 
-				2L,
-                "Bawantha",
-				LocalDate.of(1997, Month.MAY, 21),
-				"bawantha1998@gmail.com"
-			);
+                    2L,
+                    "Bawantha",
+                    LocalDate.of(1997, Month.MAY, 21),
+                    "bawantha1998@gmail.com");
 
             repository.saveAll(
-                List.of(mahesh, bawantha)
-            );
+                    List.of(mahesh, bawantha));
         };
     }
 }

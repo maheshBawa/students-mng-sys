0470d5ba829cc75a0452973fff53f1d7b10e6ee1
maheshBawa <mahesh.bawantha@revocaresolutions.com>
2023-03-14 15:21:43 +0000

    implement frontend

--- src/main/java/com/student/student/studentController.java
+++ src/main/java/com/student/student/studentController.java
@@ -3,6 +3,7 @@ package com.student.student;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
+@CrossOrigin
 public class studentController {
 
     private final StudentService studentService;
